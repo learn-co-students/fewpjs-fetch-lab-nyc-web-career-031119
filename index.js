3c7ddff253cfd9ec922c8edf9f0728732ad2c73b
Caridad Rivera <ccrive10@gmail.com>
2019-04-26 11:11:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function fetchBooks() {
-}
 
-function renderBooks(json) {
+
+
+function fetchBooks() {
+  let allBooks = ''
+
+  fetch("https://anapioficeandfire.com/api/books")
+      .then(res => res.json())
+      .then(books => {
+      allBooks = books
+   })
+ }
+
+function renderBooks(allBooks ) {
+
   const main = document.querySelector('main')
-  json.forEach(book => {
+  allBooks.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
